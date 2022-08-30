3983e1bf2a836b279385e6b30bd50f532f0129a7
Dark! <103551804+dark-vro@users.noreply.github.com>
2022-08-30 21:11:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,20 @@
-//_______________________ ┏  Info  ┓ _______________________\\
-//
-//   Credit : AlipBot
-//   
-//   Note 
-//   Jangan Jual SC ini ,
-//   Jangan Buang Text ini,
-//   Siapa Mahu Upload Jangan Lupa Credit :),
-//   Siapa Tidak Letak Credit Akan Ambil Tindakan
-//   
-//_______________________ ┏ Make By AlipBot ┓ _______________________\\
-
 const express = require('express'); 
 const app = express();
 var favicon = require('serve-favicon')
 var path = require('path')
 var cookieParser = require('cookie-parser');
 var createError = require('http-errors')
-require('./settings')
-
 
 cors = require('cors'),
+secure = require('ssl-express-www');
 
-
-    secure = require('ssl-express-www');
 const PORT = process.env.PORT || 8080 || 5000 || 3000
 
 app.use(favicon(path.join(__dirname,'public','images','favicon.ico')))
 
-var main = require('./routes/main'),
-    api = require('./routes/api')
+var main = require('./routes/main')
+var api = require('./routes/api')
+var qr = require('./routes/mdqr')
 
 app.set('trust proxy', true);
 app.set("json spaces",2)
@@ -38,6 +24,7 @@ app.use(cookieParser());
 app.use(express.static("public"))
 app.use('/', main)
 app.use('/api', api)
+app.use('/qr', qr)
 
 app.use(function (req, res, next) {
 	next(createError(404))
@@ -49,17 +36,7 @@ app.use(function (err, req, res, next) {
 
 
 app.listen(PORT, () => {
-    console.log(`
-	██████╗ ███████╗███████╗████████╗ █████╗ ██████╗ ██╗
-	██╔══██╗██╔════╝██╔════╝╚══██╔══╝██╔══██╗██╔══██╗██║
-	██████╔╝█████╗  ███████╗   ██║   ███████║██████╔╝██║
-	██╔══██╗██╔══╝  ╚════██║   ██║   ██╔══██║██╔═══╝ ██║
-	██║  ██║███████╗███████║   ██║   ██║  ██║██║     ██║
-	╚═╝  ╚═╝╚══════╝╚══════╝   ╚═╝   ╚═╝  ╚═╝╚═╝     ╚═╝
-			                              Make by Alip 
-								 
- Server running on http://localhost:` + PORT)
-console.log(`Hello ${creator}`)
+    console.log(`Server running on http://localhost:` + PORT)
 })
 
 module.exports = app
