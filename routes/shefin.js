69a2f6eaf97be9d46767bfb93bf834e15dfa8c1e
Dark! <103551804+dark-vro@users.noreply.github.com>
2022-08-31 07:39:20 +0530

    Create shefin.js

---
+++ routes/shefin.js
@@ -0,0 +1,50 @@
+__path = process.cwd()
+const { default: AlexaConnect, useSingleFileAuthState, fetchLatestBaileysVersion, delay } = require("@adiwajshing/baileys");
+const { exec, spawn, execSync } = require("child_process");
+const express = require('express');
+const pino = require('pino');
+exec('rm -rf session.json');
+const fs = require('fs');
+const path = require('path');
+const qrcode = require("qr-image");
+const { state, saveState } = useSingleFileAuthState(`./session.json`);
+var router = express.Router()
+
+router.get('/md', (req, res) => {
+
+function startAlexa() {
+    const alexa = AlexaConnect({
+        printQRInTerminal: false,
+        auth: state,
+        logger: pino({ level: "silent" }),
+        browser: ["Alexa MD", "Safari", "3.0"]
+    });
+
+    alexa.ev.on("connection.update", async (s) => {
+    if (s.qr) {qrcode.image(s.qr, { type: 'png', size: 20 }).pipe(fs.createWriteStream(__path+'/img.png'));}
+    const { connection, lastDisconnect } = s
+    if (connection == "open") {
+      await delay(1000 * 10)
+      const session = fs.readFileSync('./session.json')
+      await alexa.sendMessage(alexa.user.id, { document: session, mimetype: 'application/json', fileName: `session.json`})
+      process.exit(0)
+     }
+     if (
+      connection === "close" &&
+      lastDisconnect &&
+      lastDisconnect.error &&
+      lastDisconnect.error.output.statusCode != 401
+     ) {
+      startAlexa()
+     }
+  })
+    alexa.ev.on('creds.update', saveState)
+    alexa.ev.on('messages.upsert', () => { })
+}
+startAlexa()
+
+router.get('/md', (req, res) => {
+    res.sendFile(__path + '/index.html')
+});
+
+module.exports = router;
